6dffcb00e81817a2e0c41b9c2d7b82789efe3c2f
Pedro Bayma <pedro_bayma@outlook.com>
2021-09-30 21:51:08 -0300

    refact: refactoring urls and adding apiToken

--- lib/shared/api_url.dart
+++ lib/shared/api_url.dart
@@ -1,2 +1,3 @@
-const apiUrl = 'https://imdb-api.com/pt/API/';
+const apiMovieUrl = 'https://imdb-api.com/pt/API/Title/';
+const apiListUrl = 'https://imdb-api.com/API/Keyword/';
 const apiToken = 'k_51a2fpgu/';
